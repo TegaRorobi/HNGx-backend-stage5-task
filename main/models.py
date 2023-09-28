4a187c3e203d43ebd69bb4c20580ef2bce8d3d74
Tega Rorobi <rorobitega@gmail.com>
2023-09-28 19:07:31 +0100

    Created a model to store videos

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+	title = models.CharField(max_length=150, null=True, blank=True)
+	file = models.FileField(upload_to='videos/')
+	upload_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title or self.file.url
