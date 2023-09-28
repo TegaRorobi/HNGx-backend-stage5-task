e2f3ce6843e01bf60d27da99df5661968028e1b4
Tega Rorobi <rorobitega@gmail.com>
2023-09-28 19:14:36 +0100

    Registered the Video model in the admin, updated requirements.txt

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Video
 
 # Register your models here.
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+	class Meta:
+		model = Video 
+		list_display = 'title', 'file', 'upload_date'
