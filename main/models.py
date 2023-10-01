bd48f4bd30553bae41e205b0be942036c6324330
Tega Rorobi <rorobitega@gmail.com>
2023-10-01 16:38:15 +0100

    Modified the Video model to include transcription and last_modified fields

--- main/models.py
+++ main/models.py
@@ -5,7 +5,9 @@ from django.urls import reverse
 class Video(models.Model):
 	title = models.CharField(max_length=150, null=True, blank=True)
 	file = models.FileField(upload_to='videos/')
+	transcription = models.TextField(null=True, blank=True)
 	upload_date = models.DateTimeField(auto_now_add=True)
+	last_modified = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return self.title or self.file.url
